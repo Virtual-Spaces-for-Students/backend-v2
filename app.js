4beeb138ac455651a4f71f5e357cf0e50fd00b69
iDrunK65 <iDrunK65@gmail.com>
2022-09-17 22:07:20 +0200

    fix: routes

--- app.js
+++ app.js
@@ -21,21 +21,6 @@ app.use(session({
 }));
 app.use(passport.session());
 
-
-
-passport.use(new OAuth2Strategy({
-		authorizationURL: 'https://adfs.vss.local/adfs/oauth2/authorize',
-		tokenURL: 'https://www.example.com/adfs/.well-known/openid-configuration',
-		clientID: "3ffad8e1-3704-4d9a-993f-00026ddd3186",
-		clientSecret: "oqIbOQSD4SW1_n08m-P8DnLP2GIrwJ397mnDw9",
-		callbackURL: "http://172.18.8.2:8080/auth/callback",
-		scope: "openid profile email openid",
-	},
-	function(accessToken, refreshToken, profile, cb) {
-		cb(null, profile);
-	}
-));
-
 passport.use(
 	new SamlStrategy(
 		{
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,10 @@ const passport = require("passport");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/login', passport.authenticate('oauth2'));
+router.get('/login', passport.authenticate('saml'));
 
 router.get('/callback',
-	passport.authenticate('oauth2', { failureRedirect: '/login' }),
+	passport.authenticate('saml', { failureRedirect: '/auth/login' }),
 	function(req, res) {
 		// Successful authentication, redirect home.
 		res.json(req.user)
