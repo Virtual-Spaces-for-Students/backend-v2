a516f33a9bccdbb1ae986368bd668f124a08c973
iDrunK65 <iDrunK65@gmail.com>
2022-09-17 20:30:50 +0200

    fix: passport config

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ app.use(passport.session());
 
 passport.use(new OAuth2Strategy({
 		authorizationURL: 'https://adfs.vss.local/adfs/oauth2/authorize',
-		tokenURL: 'https://www.example.com/adfs/oauth2/token',
+		tokenURL: 'https://www.example.com/adfs/.well-known/openid-configuration',
 		clientID: "3ffad8e1-3704-4d9a-993f-00026ddd3186",
 		clientSecret: "oqIbOQSD4SW1_n08m-P8DnLP2GIrwJ397mnDw9",
 		callbackURL: "http://172.18.8.2:8080/auth/callback",
-		scope: "openid profile email",
+		scope: "openid profile email openid",
 	},
 	function(accessToken, refreshToken, profile, cb) {
 		cb(null, profile);
