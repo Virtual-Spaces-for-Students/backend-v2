1b57a406440edfcb705f86a1cf0114b25a2b8fe0
iDrunK65 <iDrunK65@gmail.com>
2022-09-17 20:23:06 +0200

    feat: add callback

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,11 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/login', passport.authenticate('oauth2'));
 
+router.get('/callback',
+	passport.authenticate('oauth2', { failureRedirect: '/login' }),
+	function(req, res) {
+		// Successful authentication, redirect home.
+		res.json(req.user)
+	}
+);
 module.exports = router;
