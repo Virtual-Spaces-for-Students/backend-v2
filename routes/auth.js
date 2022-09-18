9655d04113b2bbf62f8dac911a3e8901e39ace27
iDrunK65 <iDrunK65@gmail.com>
2022-09-18 09:27:24 +0200

    fix: debug

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,25 @@ const router = express.Router();
 router.get('/login', passport.authenticate('saml'));
 
 router.post('/callback',
-	passport.authenticate('saml', { failureRedirect: '/auth/login' }),
-	function(req, res) {
-		// Successful authentication, redirect home.
-		res.json(req.user)
-	}
-);
+	passport.authenticate('saml', { failureRedirect: '/auth/login' }), function (error, user, info) {
+		// this will execute in any case, even if a passport strategy will find an error
+		// log everything to console
+		console.log(error);
+		console.log(user);
+		console.log(info);
+
+		if (error) {
+			res.status(401).send(error);
+		} else if (!user) {
+			res.status(401).send(info);
+		} else {
+			next();
+		}
+
+		res.status(401).send(info);
+	}), (req, res) => {
+
+}
 
 router.get('/logout', async (req, res) => {
 	if (req.isAuthenticated()) {
