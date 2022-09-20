d9b5af3c7d17e05290ae16da65cdc8f18768a554
iDrunK <idrunk65@gmail.com>
2022-09-20 13:00:34 +0200

    fix: auth works

--- routes/auth.js
+++ routes/auth.js
@@ -9,20 +9,11 @@ router.post('/callback',
 	passport.authenticate('saml', { failureRedirect: '/auth/login' }), function (error, user, info) {
 		// this will execute in any case, even if a passport strategy will find an error
 		// log everything to console
-		console.log(error);
 		console.log(user);
-		console.log(info);
 
-		if (error) {
-			res.status(401).send(error);
-		} else if (!user) {
-			res.status(401).send(info);
-		} else {
-			next();
-		}
-
-		res.status(401).send(info);
 	}), (req, res) => {
+
+		res.json({ sessionID: req.sessionID, user: req.user });
 
 }
 
